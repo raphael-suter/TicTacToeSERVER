456581afbbbaf3a8662c0cc722a665eb711d2c5b
Raphael <raphael.suter@axa.ch>
2020-03-11 15:26:57 +0100

    Error handling

--- Db.js
+++ Db.js
@@ -7,60 +7,86 @@ var Db = /** @class */ (function () {
     function Db() {
         var _this = this;
         this.loadPLayer = function (req, res) {
-            _this["try"](_this.players
-                .findOne({ _id: _this.toInt(req.params._id) })
-                .then(function (data) { return res.json(data); }));
+            var _id = parseInt(req.params.id);
+            _this["try"]({
+                res: res,
+                action: _this.players.findOne({ _id: _id }),
+                onSuccess: function (result) { return res.json(result); }
+            });
+        };
+        this.savePlayer = function (req, res) {
+            var _id = parseInt(req.params.id);
+            var _a = req.params, name = _a.name, points = _a.points;
+            _this["try"]({
+                res: res,
+                action: _this.players.updateOne({ _id: _id }, { $set: { name: name, points: points } }),
+                onSuccess: 
+                // Maybe updateOne had no effect (matchedCount = 0), because there was no record found with this id.
+                // In this case a new record has to be inserted.
+                function (_a) {
+                    var matchedCount = _a.matchedCount;
+                    if (matchedCount === 0) {
+                        _this["try"]({
+                            res: res,
+                            action: _this.players.insertOne({ _id: _id, name: name, points: points })
+                        });
+                    }
+                    else {
+                        res.sendStatus(200);
+                    }
+                }
+            });
         };
         this.deletePlayer = function (req, res) {
-            _this["try"](_this.players
-                .deleteOne({ _id: _this.toInt(req.params._id) })
-                .then(res.send('Deleted.')));
+            var _id = parseInt(req.params.id);
+            _this["try"]({
+                res: res,
+                action: _this.players.deleteOne({ _id: _id })
+            });
         };
-        this["try"](mongodb_1.MongoClient
+        mongodb_1.MongoClient
             .connect('mongodb+srv://admin:m372fvzZde3z2Hk@tictactoe-yq7zv.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true })
             .then(function (client) {
             _this.players = client.db('tictactoe').collection('players');
             console.log('Connected to DB.');
             _this.startServices();
-        }));
+        })["catch"](this.printError);
     }
     Db.prototype.startServices = function () {
         var app = express();
         app.use(cors());
         [
             {
-                params: '/loadPlayer/:_id',
+                url: '/loadPlayer/:id',
                 action: this.loadPLayer
             },
             {
-                params: '/savePlayer/:_id/:name/:points',
+                url: '/savePlayer/:id/:name/:points',
                 action: this.savePlayer
             },
             {
-                params: '/deletePlayer/:_id',
+                url: '/deletePlayer/:id',
                 action: this.deletePlayer
             }
         ].forEach(function (_a) {
-            var params = _a.params, action = _a.action;
-            return app.get(params, action);
+            var url = _a.url, action = _a.action;
+            return app.get(url, action);
         });
         app.listen(8000);
         console.log('Services started.');
     };
-    Db.prototype.savePlayer = function (req, res) {
-        var _a = req.params, _id = _a._id, name = _a.name, points = _a.points;
-        _id = this.toInt(_id);
-        this.players
-            .updateOne({ _id: _id }, { $set: { name: name, points: points } })
-            .then(res.send('Updated.'))["catch"](this["try"](this.players
-            .insertOne({ _id: _id, name: name, points: points })
-            .then(res.send('Saved.'))));
+    Db.prototype["try"] = function (params) {
+        var _this = this;
+        var res = params.res, action = params.action, _a = params.onSuccess, onSuccess = _a === void 0 ? (function () { return res.sendStatus(200); }) : _a;
+        var onError = function (error) {
+            res.sendStatus(500);
+            _this.printError(error);
+        };
+        action
+            .then(onSuccess)["catch"](onError);
     };
-    Db.prototype.toInt = function (value) {
-        return parseInt(value, 10);
-    };
-    Db.prototype["try"] = function (promise) {
-        promise["catch"](function (error) { return console.log(error); });
+    Db.prototype.printError = function (error) {
+        console.log(error);
     };
     return Db;
 }());
--- Db.ts
+++ Db.ts
@@ -6,79 +6,97 @@ class Db {
   private players;
 
   constructor() {
-    this.try(
-      MongoClient
-        .connect('mongodb+srv://admin:m372fvzZde3z2Hk@tictactoe-yq7zv.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true })
-        .then(client => {
-          this.players = client.db('tictactoe').collection('players');
-          console.log('Connected to DB.');
+    MongoClient
+      .connect('mongodb+srv://admin:m372fvzZde3z2Hk@tictactoe-yq7zv.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true })
+      .then(client => {
+        this.players = client.db('tictactoe').collection('players');
+        console.log('Connected to DB.');
 
-          this.startServices();
-        })
-    );
+        this.startServices();
+      })
+      .catch(this.printError);
   }
 
-  private startServices(): void {
+  private startServices() {
     const app = express();
     app.use(cors());
 
     [
       {
-        params: '/loadPlayer/:_id',
+        url: '/loadPlayer/:id',
         action: this.loadPLayer
       },
       {
-        params: '/savePlayer/:_id/:name/:points',
+        url: '/savePlayer/:id/:name/:points',
         action: this.savePlayer
       },
       {
-        params: '/deletePlayer/:_id',
+        url: '/deletePlayer/:id',
         action: this.deletePlayer
       }
-    ].forEach(({ params, action }) => app.get(params, action));
+    ].forEach(({ url, action }) => app.get(url, action));
 
     app.listen(8000);
     console.log('Services started.');
   }
 
-  public loadPLayer = (req, res): void => {
-    this.try(
-      this.players
-        .findOne({ _id: this.toInt(req.params._id) })
-        .then(data => res.json(data))
-    );
+  public loadPLayer = (req, res) => {
+    const _id = parseInt(req.params.id);
+
+    this.try({
+      res,
+      action: this.players.findOne({ _id }),
+      onSuccess: result => res.json(result)
+    });
   }
 
-  public savePlayer(req, res): void {
-    let { _id, name, points } = req.params;
-    _id = this.toInt(_id);
+  public savePlayer = (req, res) => {
+    const _id = parseInt(req.params.id);
+    const { name, points } = req.params;
 
-    this.players
-      .updateOne({ _id }, { $set: { name, points } })
-      .then(res.send('Updated.'))
-      .catch(
-        this.try(
-          this.players
-            .insertOne({ _id, name, points })
-            .then(res.send('Saved.'))
-        )
-      );
+    this.try({
+      res,
+      action: this.players.updateOne({ _id }, { $set: { name, points } }),
+      onSuccess:
+        // Maybe updateOne had no effect (matchedCount = 0), because there was no record found with this id.
+        // In this case a new record has to be inserted.
+        ({ matchedCount }) => {
+          if (matchedCount === 0) {
+            this.try({
+              res,
+              action: this.players.insertOne({ _id, name, points })
+            });
+          } else {
+            res.sendStatus(200);
+          }
+        }
+    });
   }
 
-  public deletePlayer = (req, res): void => {
-    this.try(
-      this.players
-        .deleteOne({ _id: this.toInt(req.params._id) })
-        .then(res.send('Deleted.'))
-    );
+  public deletePlayer = (req, res) => {
+    const _id = parseInt(req.params.id);
+
+    this.try({
+      res,
+      action: this.players.deleteOne({ _id })
+    });
   }
 
-  private toInt(value: string): number {
-    return parseInt(value, 10);
+  private try(params: { res, action, onSuccess?}) {
+    const { res, action, onSuccess = (() => res.sendStatus(200)) } = params;
+
+    const onError = (error) => {
+      res.sendStatus(500);
+      this.printError(error);
+    }
+
+    action
+      .then(onSuccess)
+      .catch(onError);
   }
 
-  private try(promise: Promise<String>): void {
-    promise.catch(error => console.log(error));
+  private printError(error) {
+    console.log(error);
   }
 }
 
